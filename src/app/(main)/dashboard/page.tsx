e739e83c81d22ed2d8e77b4bdc54f722ccf45047
Virendra <virendrasonivs07@gmail.com>
2025-01-06 03:01:42 +0530

    Add session types, utility functions, and dashboard data retrieval; implement sessions table component

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -11,8 +11,9 @@ import {
   ResponsiveContainer,
   XAxis,
   YAxis,
-  Tooltip,
+  Tooltip as Tool,
   Cell,
+  Legend,
 } from "recharts";
 import {
   Clock,
@@ -30,6 +31,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { format } from "date-fns";
 
 import { Progress } from "@/components/ui/progress";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -41,17 +43,14 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { RainbowButton } from "@/components/ui/rainbow-button";
-
-// Mock data for charts
-const moodData = [
-  { day: "Mon", score: 4 },
-  { day: "Tue", score: 7 },
-  { day: "Wed", score: 5 },
-  { day: "Thu", score: 6 },
-  { day: "Fri", score: 8 },
-  { day: "Sat", score: 9 },
-  { day: "Sun", score: 7 },
-];
+import { useAsyncEffect } from "@/hooks/useAysncEffect";
+import { GetDashboardData } from "@/services/dashboard/action";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { SessionTable } from "@/components/Dashboard/SessionsTable";
 
 const topicsData = [
   { name: "Anxiety", value: 30 },
@@ -61,20 +60,56 @@ const topicsData = [
   { name: "Self-esteem", value: 10 },
 ];
 
-const progressData = [
-  { week: "Week 1", score: 3 },
-  { week: "Week 2", score: 4 },
-  { week: "Week 3", score: 5 },
-  { week: "Week 4", score: 6 },
-  { week: "Week 5", score: 7 },
-  { week: "Week 6", score: 8 },
-];
+interface DashboardData {
+  session_count: number;
+  sessions: [
+    {
+      session_id: string;
+      min_created_at: string;
+      user_word_count: number;
+      assistant_word_count: number;
+      duration_seconds: number;
+    }
+  ];
+  streaks: {
+    current_streak: number;
+    max_streak: number;
+  };
+  avg_session_duration: number;
+  user_avg_word_count: number;
+  assistant_avg_word_count: number;
+}
 
 const COLORS = ["#3B82F6", "#10B981", "#F59E0B", "#EF4444", "#8B5CF6"];
 
 export default function Dashboard() {
-  const [consistencyStreak] = useState(5);
   const [timeFrame, setTimeFrame] = useState("week");
+  const [dashboardData, setDashboardData] = useState<DashboardData>({
+    session_count: 0,
+    sessions: [
+      {
+        session_id: "",
+        user_word_count: 0,
+        min_created_at: "",
+        assistant_word_count: 0,
+        duration_seconds: 0,
+      },
+    ],
+    streaks: {
+      current_streak: 0,
+      max_streak: 0,
+    },
+    avg_session_duration: 0,
+    user_avg_word_count: 0,
+    assistant_avg_word_count: 0,
+  });
+
+  useAsyncEffect(async () => {
+    const response = await GetDashboardData();
+    setDashboardData(response);
+  }, []);
+
+  console.log(dashboardData);
 
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
@@ -110,8 +145,10 @@ export default function Dashboard() {
             <MessageCircle className="h-4 w-4 text-blue-500" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-gray-800">24</div>
-            <p className="text-xs text-green-600">+2 from last week</p>
+            <div className="text-4xl font-bold text-gray-800">
+              {dashboardData.session_count}
+            </div>
+            <p className="text-xs text-green-600 mt-2">+2 from last week</p>
           </CardContent>
         </Card>
         <Card className="bg-white shadow-lg">
@@ -123,12 +160,15 @@ export default function Dashboard() {
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold text-gray-800">
-              {consistencyStreak} days
+              {dashboardData.streaks.current_streak} days
             </div>
             <Progress
-              value={consistencyStreak * 10}
+              value={dashboardData.streaks.current_streak * 10}
               className="mt-2 bg-blue-100"
             />
+            <p className="text-xs text-blue-600 font-semibold mt-2">
+              Maximum Streaks: {dashboardData.streaks.max_streak} Days
+            </p>
           </CardContent>
         </Card>
         <Card className="bg-white shadow-lg">
@@ -139,7 +179,12 @@ export default function Dashboard() {
             <Clock className="h-4 w-4 text-blue-500" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-gray-800">18 minutes</div>
+            <div className="text-2xl font-bold text-gray-800">
+              {(
+                Number(dashboardData.avg_session_duration.toFixed(2)) / 60
+              ).toFixed(2)}{" "}
+              minutes
+            </div>
             <p className="text-xs text-gray-500">Optimal: 15-25 minutes</p>
           </CardContent>
         </Card>
@@ -155,28 +200,80 @@ export default function Dashboard() {
             <p className="text-xs text-green-600">Since starting therapy</p>
           </CardContent>
         </Card>
+        <Tooltip>
+          <TooltipTrigger>
+            <Card className="bg-white shadow-lg">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2 text-gray-600">
+                  <MessageCircle className="h-4 w-4" />
+                  Average Words Count (You)
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex items-end gap-1">
+                <p className="text-4xl font-bold text-center">
+                  {dashboardData.user_avg_word_count.toFixed(2)}
+                </p>
+                <p className="text-sm font-medium text-end mb-1">
+                  words per session
+                </p>
+              </CardContent>
+            </Card>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>The average number of messages exchanged per session</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger>
+            <Card className="bg-white shadow-lg">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2 text-gray-600">
+                  <MessageCircle className="h-4 w-4" />
+                  Average Words Count (Euphonia)
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex items-end gap-1">
+                <p className="text-4xl font-bold text-center">
+                  {dashboardData.assistant_avg_word_count.toFixed(2)}
+                </p>
+                <p className="text-sm font-medium text-end mb-1">
+                  words per session
+                </p>
+              </CardContent>
+            </Card>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>The average number of messages exchanged per session</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
 
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7 mt-8">
         <Card className="col-span-4 bg-white shadow-lg">
           <CardHeader>
-            <CardTitle className="text-gray-800">Mood Tracker</CardTitle>
+            <CardTitle className="text-gray-800">
+              Session Duration Tracker
+            </CardTitle>
             <CardDescription className="text-gray-600">
-              Your mood scores over time
+              Duration of your therapy sessions
             </CardDescription>
           </CardHeader>
           <CardContent className="pl-2">
             <ResponsiveContainer width="100%" height={350}>
-              <BarChart data={moodData}>
-                <XAxis dataKey="day" stroke="#6B7280" />
+              <BarChart data={dashboardData.sessions}>
+                <XAxis
+                  // dataKey="min_created_at"
+                  stroke="#6B7280"
+                  reversed={true}
+                />
                 <YAxis stroke="#6B7280" />
-                <Tooltip
+                <Tool
                   contentStyle={{
                     backgroundColor: "white",
                     border: "1px solid #E5E7EB",
                   }}
                 />
-                <Bar dataKey="score" fill="#3B82F6" />
+                <Bar dataKey="duration_seconds" fill="#3B82F6" />
               </BarChart>
             </ResponsiveContainer>
           </CardContent>
@@ -205,12 +302,12 @@ export default function Dashboard() {
                 >
                   {topicsData.map((entry, index) => (
                     <Cell
-                      key={`cell-${index}`}
+                      key={`cell-${index}-${entry.name}`}
                       fill={COLORS[index % COLORS.length]}
                     />
                   ))}
                 </Pie>
-                <Tooltip
+                <Tool
                   contentStyle={{
                     backgroundColor: "white",
                     border: "1px solid #E5E7EB",
@@ -232,10 +329,11 @@ export default function Dashboard() {
           </CardHeader>
           <CardContent className="pl-2">
             <ResponsiveContainer width="100%" height={350}>
-              <LineChart data={progressData}>
+              <LineChart data={dashboardData.sessions}>
                 <XAxis dataKey="week" stroke="#6B7280" />
                 <YAxis stroke="#6B7280" />
-                <Tooltip
+                <Legend />
+                <Tool
                   contentStyle={{
                     backgroundColor: "white",
                     border: "1px solid #E5E7EB",
@@ -243,9 +341,17 @@ export default function Dashboard() {
                 />
                 <Line
                   type="monotone"
-                  dataKey="score"
+                  dataKey="user_word_count"
                   stroke="#10B981"
                   strokeWidth={2}
+                  name="Users Words Count"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="assistant_word_count"
+                  stroke="#8884d8"
+                  strokeWidth={2}
+                  name="Assistant Words Count"
                 />
               </LineChart>
             </ResponsiveContainer>
@@ -376,6 +482,59 @@ export default function Dashboard() {
               </div>
             </div>
           </CardContent>
+        </Card>
+      </div>
+      <Card className="mt-8">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            <MessageCircle className="h-4 w-4" />
+            Recent Chat Sessions
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {dashboardData.sessions.slice(0, 10).map((session) => (
+              <li
+                key={session.session_id}
+                className="flex justify-between items-center"
+              >
+                <div>
+                  <p className="font-semibold">
+                    Duration: {(session.duration_seconds / 60).toFixed(2)} min -
+                    Messages: {session.user_word_count}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {session.min_created_at &&
+                      format(session?.min_created_at, "MMMM d, yyyy h:mm a")}
+                  </p>
+                  {/* <p className="text-sm mt-1">
+                    Topics: {session.topicsCovered.join(", ")}
+                  </p> */}
+                  {/* <p
+                    className={`text-sm ${
+                      session.sentimentScore > 0
+                        ? "text-green-500"
+                        : session.sentimentScore < 0
+                        ? "text-red-500"
+                        : ""
+                    }`}
+                  >
+                    Sentiment: {session.sentimentScore.toFixed(2)}
+                  </p> */}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+      <div className="mx-auto mt-8 rounded-xl">
+        <Card className="bg-white shadow-lg">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2">
+              Users Session Table
+            </CardTitle>
+          </CardHeader>
+          <SessionTable data={dashboardData?.sessions} />
         </Card>
       </div>
     </div>
