b88ffbdb819cc9ba096dbcf6190c1372cb7f64c8
viren soni <virendra.soni@recykal.com>
2025-01-10 10:58:56 +0530

    Add PostHog analytics integration and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 import { Toaster } from "@/components/ui/toaster";
+import { CSPostHogProvider } from "@/provider/PosthogProvider";
 
 // const quicksand = Poppins({ subsets: ["latin"], weight: "400", display: "swap" });
 const quicksand = Poppins({
@@ -29,34 +30,36 @@ export default function RootLayout({
       <head>
         <link rel="canonical" href="https://euphonia.me/" />
       </head>
-      <body className={quicksand.className} suppressHydrationWarning>
-        <NextTopLoader
-          color="#000"
-          initialPosition={0.08}
-          crawlSpeed={200}
-          height={3}
-          crawl={true}
-          showSpinner={true}
-          easing="ease"
-          speed={200}
-          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+      <CSPostHogProvider>
+        <body className={quicksand.className} suppressHydrationWarning>
+          <NextTopLoader
+            color="#000"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={true}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
   <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-          zIndex={1600}
-          showAtBottom={false}
-        />
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Analytics />
-          <SpeedInsights />
-          <Toaster />
-        </ThemeProvider>
-      </body>
+            zIndex={1600}
+            showAtBottom={false}
+          />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Analytics />
+            <SpeedInsights />
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
