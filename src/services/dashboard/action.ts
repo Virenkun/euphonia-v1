39905f34481951d611921e18adb388a1eb02cf89
Viren Soni <virendrasonivs07@gmail.com>
2025-01-14 13:57:55 +0530

    Remove debug console logs from layout and dashboard action files

--- src/services/dashboard/action.ts
+++ src/services/dashboard/action.ts
@@ -25,8 +25,6 @@ export async function GetDashboardData() {
     }
   );
 
-  console.log("sesss", sessions.length);
-
   const { data: streks, error: streaksError } = await supabase.rpc(
     "get_user_streaks",
     { user_uuid: auth_id }
