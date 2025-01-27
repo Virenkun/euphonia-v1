e0201e9eaca5219757df9983d15df343c4d893a5
viren soni <virendrasonivs07@gmail.com>
2025-01-27 16:58:16 +0530

    Enhance loading component with animated messages and improve billing session handling

--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -1,5 +1,9 @@
 import BillingPage from "@/components/Billing/BillingPage";
-import { getBillingDetails, getPaymentList } from "@/services/users/action";
+import {
+  getBillingDetails,
+  getPaymentList,
+  getUserInfo,
+} from "@/services/users/action";
 import { createClient } from "@/utils/supabase/server";
 import React from "react";
 
@@ -7,12 +11,13 @@ async function Page() {
   const supabase = await createClient();
   const currentPlan = await getBillingDetails();
   const paymentList = await getPaymentList();
-  const { data: sessionsUsed } = await supabase.rpc("count_unique_sessions");
+  const userInfo = await getUserInfo();
   const { data: allPlans } = await supabase.from("pricing").select("*");
   return (
     <BillingPage
       currentPlan={currentPlan?.plan}
-      sessionUsed={sessionsUsed}
+      sessionUsed={userInfo?.session_used}
+      allotedSessions={userInfo?.alloted_sessions}
       allPlans={allPlans || []}
       paymentList={Array.isArray(paymentList) ? paymentList : []}
     />
